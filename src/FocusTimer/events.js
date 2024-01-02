6410dd5bbc0cc682ac445fcf6c90536ef909f088
Ítalo Oliveira <130611493+nasdrovin@users.noreply.github.com>
2024-01-02 16:49:54 -0300

    Add files via upload

---
+++ src/FocusTimer/events.js
@@ -0,0 +1,23 @@
+import { controls } from './elements.js'
+import { clima } from './elements.js'
+import * as actions from './actions.js'
+
+export function registerControls() {
+    controls.addEventListener('click', (event) => {
+        const action = event.target.dataset.action
+        if(typeof actions[action] != 'function') {
+            return
+        }
+        
+        actions[action]()
+    })
+
+    clima.addEventListener('click', (event) => {
+        const action = event.target.dataset.action
+        if(typeof actions[action] != 'function') {
+            return
+        }
+        
+        actions[action]()
+    })
+}
